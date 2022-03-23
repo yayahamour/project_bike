ecf4d9b197c5127bb985fd8e0627302f797c3460
ayoubhaddou <ayoubhaddou1@gmail.com>
2022-03-23 18:45:11 +0100

    correction affichage + pipeline manuel ok

--- pipeline.py
+++ pipeline.py
@@ -6,9 +6,17 @@ import datetime as dt
 
 # https://opendata.lillemetropole.fr/explore/dataset/vlille-realtime/table/
 
+# Enelever jour de vacance car on le calcule automatiquement avec la date ? 
+# Refaire Holiday et les jours ferié pour la piepline de france 
+
 # faire graph count vélo par jour des 20 jours précédents la requêtes + 10 des jours suivants 
 
 # Phrase impliquant tout les facteurs de la prédictions. 
+
+
+
+
+# Pipeline pour la prediction automatique pour Washington DC : 
 
 def run_df_dc():
     df = pd.read_csv('./data/train.csv')
@@ -93,6 +101,10 @@ def run_df_dc():
     return df_app
 
 
+
+
+# Pipeline pour la prediction automatique pour Lille : 
+
 def run_df_lille():
     df = pd.read_csv('./data/train.csv')
     df.drop('Unnamed: 0', axis=1, inplace = True)
@@ -172,5 +184,84 @@ def run_df_lille():
     
     df_app.drop('weather2', axis=1, inplace=True)
     df_app.drop('count', axis=1, inplace=True)
+    
+    return df_app
+
+
+
+
+# Pipeline pour la prédiction personnalisée : 
+
+def run_df_dc_personalised(date, time, day, weather, temp, temp_feel, humidity, windspeed):
+    df = pd.read_csv('./data/train.csv')
+    df.drop('Unnamed: 0', axis=1, inplace = True)
+    df.drop('datetime', axis=1, inplace=True)
+    df.drop(columns=['registered', 'casual'], index=1, inplace=True)
+    df_app = pd.DataFrame(columns=df.keys(), index=np.arange(1,5,1))
+
+    # Day of year 
+    df_app['day']  = date
+    df_app['day']  = pd.to_datetime(df_app['day']).dt.dayofyear
+    time = str(time) 
+    df_app['hour'] = int(time[-8:-6])
+
+
+    # Season 
+    df_app['season'][df_app['day'] < 172 ] = 1
+    df_app['season'][(df_app['day'] >= 172) & (df_app['day'] < 264 ) ] = 2
+    df_app['season'][(df_app['day'] >= 264) & (df_app['day'] < 356 ) ] = 3
+    df_app['season'][(df_app['day'] >= 356) | (df_app['day'] < 172 ) ] = 4
+
+
+    # Holliday 
+    df_app['holiday'] = 0 
+    df_app['holiday'][(df_app['day'] >= 357) & (df_app['day'] <= 2 ) ] = 1
+    df_app['holiday'][(df_app['day'] >= 52) & (df_app['day'] <= 56 ) ] = 1
+    df_app['holiday'][(df_app['day'] >= 69) & (df_app['day'] <= 70 ) ] = 1
+    df_app['holiday'][(df_app['day'] >= 101) & (df_app['day'] <= 108 ) ] = 1
+    df_app['holiday'][(df_app['day'] >= 185) & (df_app['day'] <= 240 ) ] = 1
+    df_app['holiday'][(df_app['day'] >= 279) & (df_app['day'] <= 283 ) ] = 1
+    df_app['holiday'][(df_app['day'] >= 315) & (df_app['day'] <= 329 ) ] = 1
+
+    # Férié 
+    df_app['holiday'][df_app['day'] == 17 ] = 1
+    df_app['holiday'][df_app['day'] == 26 ] = 1
+    df_app['holiday'][df_app['day'] == 150 ] = 1
+    df_app['holiday'][df_app['day'] == 129 ] = 1
+    df_app['holiday'][df_app['day'] == 305 ] = 1
+    df_app['holiday'][df_app['day'] == 308 ] = 1
+
+
+    df_app['temp'] = int(temp)
+    df_app['atemp'] = int(temp_feel)
+    df_app['humidity'] = int(humidity)
+    df_app['windspeed'] = int(windspeed)
+    df_app['weather'] = str(weather)
+        
+        
+    # A refa
+    df_app['workingday'] = 0
+    for i in np.arange(1,365,7):
+        df_app['workingday'][(df_app['day'] == i) | (df_app['day'] == i) + 1 ] = 1
+        
+        
+        
+    df_app.rename(columns = {'weather' : 'weather2'}, inplace = True)
+    df_app.insert(4, 'weather', "")
+
+
+    # df_app['weather'][df_app['weather'].str.lower().isin(['clear', 'few clouds', 'partly cloudy'])] = 1
+    df_app['weather'][df_app['weather2'].str.lower() == 'ciel dégagé/légérement nuageux'] = 1
+    df_app['weather'][df_app['weather2'].str.lower() == 'brume/nuageux'] = 2
+    df_app['weather'][df_app['weather2'].str.lower() == 'légère pluie/légére chute de neige/nuages eparpillé'] = 3
+    df_app['weather'][df_app['weather2'].str.lower() == 'forte pluie/chute de neige/brouilard/orage'] = 4
+
+    df_app['weather'] = df_app['weather'].astype(int)
+    df_app['season'] = df_app['season'].astype(int)
+    
+    df_app.drop('weather2', axis=1, inplace=True)
+    df_app.drop('count', axis=1, inplace=True)
+    
+
     
     return df_app
