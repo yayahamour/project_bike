8affb174e20445c690008b5b535572d16c00f0bf
VAlGrus <grusonvalent@gmail.com>
2022-03-30 13:26:14 +0200

    maj

--- database.py
+++ database.py
@@ -1,21 +1,40 @@
+import pandas as pd
 import sqlite3
 
-DB = sqlite3.connect('projet_bike')
+DB = sqlite3.connect('projet_bike.db')
 
-
-def create_table():
-    #creation d'une table vide 
+def create_table(name):
     cursor = DB.cursor()
     cursor.execute("""
-        CREATE TABLE IF NOT EXISTS users(
+        CREATE TABLE IF NOT EXISTS """ + name +"""(
         id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
-        name TEXT,
-        age INTERGER
-        )
-        """)
+        datetime TIMESERIES,
+        season INTERGER,
+        holiday INTEGER,
+        workingday INTEGER,
+        weather INTEGER,
+        temp FLOAT,
+        atemp FLOAT,
+        humidity INTEGER,
+        windspeed FLOAT,
+        casual INTEGER,
+        registered INTEGER,
+        count INTEGER
+        );""")
     DB.commit()
 
-    #insertion des données
-     
-     
-db.close()
+def add_table(name, datetime,season,holiday,workingday,weather,temp,atemp,humidity,windspeed,casual,registered,count):
+    cursor = DB.cursor()
+    data = "INSERT INTO "+name+" (datetime,season,holiday,workingday,weather,temp,atemp,humidity,windspeed,casual,registered,count) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)"
+    value = (datetime,season,holiday,workingday,weather,temp,atemp,humidity,windspeed,casual,registered,count)
+    cursor.execute (data, value)
+    DB.commit()
+    cursor.close()
+
+def del_row(name,id):
+    cursor = DB.cursor()
+    data = "DELETE FROM " + name + " WHERE id = "+ id + ""
+    cursor.execute(data)
+    DB.commit()
+    cursor.close()
+    DB.close()
--- myclass.py
+++ myclass.py
@@ -7,13 +7,10 @@ import pandas as pd
 import numpy as np 
 from PIL import Image
 from Pipeline.pipeline_df import run_df_dc , run_df_lille, run_df_dc_personalised
-<<<<<<< HEAD
-=======
 import seaborn as sns 
 import altair as alt 
 import matplotlib.pyplot as plt 
 
->>>>>>> d9c8157e4d39e9ae371ae7e5c4f84da706a19b13
 
 class Interface:
     
